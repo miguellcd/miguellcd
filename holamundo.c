2a382b60c7a01461077a3f37193f496ca2d69716
miguellcd <miguellcd10@gmail.com>
2016-10-07 14:24:37 -0500

    suma de dos variables

--- holamundo.c
+++ holamundo.c
@@ -3,6 +3,17 @@
 
 int main()
 {
-    printf("Hello world!\n");
+  int numero = 0;
+  int numero1 = 0;
+  int resultado = 0;
+
+    printf("ingrese un numero");
+    scanf("%d\n",numero );
+
+    printf("ingrese el segundo numero" );
+    scanf("%d",numero1 );
+
+    resultado = numero + numero1;
+     printf("resultado de la operacion", resultado );
     return 0;
 }
